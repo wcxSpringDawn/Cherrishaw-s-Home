ca0e2d8e148616abefbb2f5dfc631edbc5509374
春晓 王 <WFwcx888@outlook.com>
2025-04-08 22:15:01 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -55,7 +55,7 @@ export const getHitokoto = async () => {
 
 // 获取高德地理位置信息
 export const getAdcode = async (key) => {
-  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
+  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}&ip=118.31.12.119`);
   return await res.json();
 };
 
