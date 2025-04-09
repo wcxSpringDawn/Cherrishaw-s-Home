d685bded19f33ee3789a0a9716bd4fa346d48073
春晓 王 <WFwcx888@outlook.com>
2025-04-09 23:32:28 +0800

    update

--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -21,7 +21,7 @@
     <Transition name="el-fade-in-linear" mode="out-in">
       <div :key="hitokotoData.text" class="content" @click="updateHitokoto">
         <span class="text">{{ hitokotoData.text }}</span>
-        <span class="from">-「&nbsp;{{ hitokotoData.from }}&nbsp;」</span>
+        <!-- <span class="from">-「&nbsp;{{ hitokotoData.from }}&nbsp;」</span> -->
       </div>
     </Transition>
   </div>
